983521a6d5607e565c59d4785dd6875a7b285c24
cephora52 <cephora52@gamil.com>
2025-11-19 11:41:28 +0100

    creation du projet

---
+++ src/main/java/com/example/tpgestionproduit/TpGestionProduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpgestionproduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGestionProduitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpGestionProduitApplication.class, args);
+    }
+
+}
